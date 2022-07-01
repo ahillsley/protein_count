e78c49fda57325e83010053464c98076f5aba3ce
ahillsley <avhillsley@utexas.edu>
2022-07-01 15:22:31 -0400

    first attempt at full image processing pipeline '

--- for_promap/fit_real_data.py
+++ for_promap/fit_real_data.py
@@ -5,6 +5,7 @@ from FluorescenceModel import ModelParams
 from TraceModel import TraceModel
 from IntensityTrace import IntensityTrace
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 image_file_path = '../../Images/0525_5nM_1.tif'
@@ -45,10 +46,10 @@ def background_simple(img, trace, x, y, tile=7):
     d = x+tile
     if d > img.shape[1]:
         d = img.shape[1]
-        
+
     for i in range(len(trace)):
         baseline[i] = np.mean(img[a:b,c:d,i])
-    
+
     background_subtracted = trace - baseline
     return background_subtracted
 
@@ -62,81 +63,37 @@ def clean_spots(spots, img):
                                  axis = 1)
     return clean_spots
 
-
-img = read_image(image_file_path)
-spots = pd.read_csv(spots_file_path)
-trace = pixel_t_trace(img, 39, 91, 3)
-
-intensity_model = IntensityTrace(trace, 0.1)
-scale = intensity_model.xy_normalize()
-
-p_on_estimate, p_off_estimate = intensity_model.fit_grid_search(points=10, p_on_max=1e-2)
-trace_params = ModelParams(p_on_estimate, p_off_estimate)
-trace_model = TraceModel(trace_params, 0.1, len(intensity_model.trace))
-on_intensity, on_noise, background_noise, p_on_estimate, p_off_estimate, states = \
-                    trace_model.fit_viterbi(intensity_model.trace, 1)
-        
-
-
-                    
-def image_fitting(image, spots):
-    ''' 
-    Args:
-        Image: np_array
-            a stack of images in shape (y, x, time)
-        
-        spots: np_array, list
-            list of spots in format y, x
-    '''
-    spots = spots.astype('int')
-    p_values = np.zeros((np.max(spots.shape), 4))
-    for s in range(len(p_values)):
-        trace = pixel_t_trace(image, spots[0,s], spots[1,s],3)
-        intensity_model = IntensityTrace(trace, 0.1)
-        scale = intensity_model.xy_normalize()
-        p_on_grid, p_off_grid = intensity_model.fit_grid_search(points=10, p_on_max=1e-2)
-        trace_params = ModelParams(p_on_estimate, p_off_estimate)
-        trace_model = TraceModel(trace_params, 0.1, len(intensity_model.trace))
-        on_intensity, on_noise, background_noise, p_on_vit, p_off_vit, states = \
-                            trace_model.fit_viterbi(intensity_model.trace, 1)
-                            
-        p_values[s,:] = p_on_grid, p_off_grid, p_on_vit, p_off_vit
-        
-        print(p_values[s,:])
-        
-    return p_values
-        
-
 img = read_image(image_file_path)
 spots = pd.read_csv(spots_file_path)
 yx_spots = clean_spots(spots, img)
-spot_num = 0
-p_values = np.zeros((np.max(yx_spots.shape), 4))
-#for spot_num in range(len(p_values)):
-for spot_num in range(10):
-    trace = pixel_t_trace(img, yx_spots[spot_num, 0], yx_spots[spot_num, 1],3)
-    bs_trace = background_simple(img, trace, yx_spots[spot_num, 1],yx_spots[spot_num, 0])
-    intensity_model = IntensityTrace(bs_trace, 0.1)
-    scale = intensity_model.xy_normalize()
 
-    p_on_grid, p_off_grid = intensity_model.fit_grid_search(points=10, p_on_max=1e-2)
-    trace_params = ModelParams(p_on_grid, p_off_grid)
-    trace_model = TraceModel(trace_params, 0.1, len(intensity_model.scaled_trace))
-    on_intensity, on_noise, background_noise, p_on_vit, p_off_vit, states = \
-                     trace_model.fit_viterbi(intensity_model.scaled_trace, 1)
-    #plt.plot(states); plt.plot(intensity_model.scaled_trace)      
-    print(spot_num)
-    print(f'from grid_search {np.around(p_on_grid,4)} / {np.around(p_off_grid,4)}- - - - from viterbi {np.around(p_on_vit,4)} / {np.around(p_off_vit,4)}')
-    p_values[spot_num,:] = p_on_grid, p_off_grid, p_on_vit, p_off_vit
-    del trace, bs_trace, intensity_model, scale, p_on_grid, p_off_grid, \
-        trace_params, trace_model,  on_intensity, on_noise, background_noise, \
+def process_image(img, yx_spots):
+    p_values = np.zeros((np.max(yx_spots.shape), 4))
+    for spot_num in range(len(p_values)):
+        trace = pixel_t_trace(img, yx_spots[spot_num, 0], yx_spots[spot_num, 1],3)
+        bs_trace = background_simple(img, trace, yx_spots[spot_num, 1],yx_spots[spot_num, 0])
+        intensity_model = IntensityTrace(bs_trace, 0.1)
+        scale = intensity_model.xy_normalize()
+
+        p_on_grid, p_off_grid = intensity_model.fit_grid_search(points=10, p_on_max=1e-2)
+        trace_params = ModelParams(p_on_grid, p_off_grid)
+        trace_model = TraceModel(trace_params, 0.1, len(intensity_model.scaled_trace))
+        on_intensity, on_noise, background_noise, p_on_vit, p_off_vit, states = \
+                         trace_model.fit_viterbi(intensity_model.scaled_trace, 1)
+        #plt.plot(states); plt.plot(intensity_model.scaled_trace)      
+        print(f'{spot_num} grid_search {np.around(p_on_grid,4)} / {np.around(p_off_grid,4)}- - - -viterbi {np.around(p_on_vit,4)} / {np.around(p_off_vit,4)}')
+        p_values[spot_num,:] = p_on_grid, p_off_grid, p_on_vit, p_off_vit
+        del trace, bs_trace, intensity_model, scale, p_on_grid, p_off_grid, \
+            trace_params, trace_model,  on_intensity, on_noise, background_noise, \
             p_on_vit, p_off_vit, states
-                    
-                    
-for spot_num in range(len(yx_spots)):
-    trace = pixel_t_trace(img, yx_spots[spot_num, 0], yx_spots[spot_num, 1],3)
-    plt.plot(trace)
-    plt.figure()
+    return p_values               
 
+p_values = process_image(img, yx_spots)
+parameters = pd.DataFrame(p_values, columns=['p_on_grid', 'p_off_grid', 'p_on_vit','p_off_vit'])
 
+colors = ['#00CD91','#E7A20B','#FE8A80', '#006E61']
+g = sns.boxplot(data=parameters,
+            linewidth=2.5,
+            palette=(colors))
+f = sns.stripplot(data=parameters, color= 'k', s=3)
 
