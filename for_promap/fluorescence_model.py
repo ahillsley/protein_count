f09c10aaf2e567b14bc5d7d216efe4deab532aac
ahillsley <avhillsley@utexas.edu>
2022-07-13 11:36:31 -0400

    changed fluoresccence model to work in log space

--- for_promap/fluorescence_model.py
+++ for_promap/fluorescence_model.py
@@ -4,9 +4,13 @@ from pomegranate import GeneralMixtureModel, LogNormalDistribution
 
 
 class ModelParams:
-    def __init__(self, u=1, sigma=0.1, mean_background=1, sigma_background=0.1,
+    def __init__(self, 
+                 log_mean_intensity=1, 
+                 sigma=0.1, 
+                 mean_background=1, 
+                 sigma_background=0.1,
                  label_eff=1):
-        self.u = u
+        self.log_mean_intensity = log_mean_intensity
         self.sigma = sigma
         self.mean_background = mean_background
         self.sigma_background = sigma_background
@@ -36,7 +40,7 @@ class FluorescenceModel:
 
     def __init__(self, model_params):
 
-        self.mu = model_params.u
+        self.log_mean_intensity = model_params.log_mean_intensity
         self.sigma = model_params.sigma
         self.sigma2 = model_params.sigma**2
         self.mean_background = model_params.mean_background
@@ -45,70 +49,144 @@ class FluorescenceModel:
         self.label_eff = model_params.label_eff
 
 
+
+    
     def sample_x_i_given_z_i(self, z):
-        '''
-        sample x from lognormal distribution around z
+        
+        mean = z *self.log_mean_intensity
+        
+        signal = np.random.normal(mean, self.sigma)
+        
+        return self._bring_out(signal)
+    
+    def p_x_i_given_z_i(self, x_i, z):
+        
+        x = self._bring_in(x_i)
+        
+        mean = z * self.log_mean_intensity
+        
+        result = integrate.romberg(lambda x: self._normal(x, mean, self.sigma2),
+                                   x, x + (1/256))
+        
+        return result
 
-        Args:
 
-            z (int):
-                the hidden state, or number of bound fluorophores
+    def _normal(self, x, mu, sigma2):
+        
+        return 1.0 / (np.sqrt(2.0 * np.pi * sigma2)) * \
+                np.exp(-(x - mu)**2/(2.0 * sigma2))
 
-        '''
+    def _bring_in(self, x):
+        return np.log(x)
+        
+    def _bring_out(self, x):
+        return np.exp(x)
 
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+    def sample_x_i_given_z_i(self, z):
         
         if z == 0:
             signal = 0
+        
         else:
             mu = np.log(self.mu * z)
-            mean = np.exp(mu + self.sigma2 / 2)
-            signal = np.random.lognormal(mean, self.sigma)
+            #mean = np.exp(mu * self.sigma2 / 2)
+            signal = np.random.lognormal(mu, self.sigma)
         
-        background = np.random.lognormal(self.mean_background,
-                                         self.sigma_background)
-
+        background = np.random.lognormal(self.mean_background, 
+                                      self.sigma_background)
+        
         return signal + background
+            
+        
+    def norm_sample_x_i_given_z_i(self, z):
+        '''
+#        sample x from lognormal distribution around z
+
+ #       Args:
+
+  #          z (int):
+   #             the hidden state, or number of bound fluorophores
+
+'''
+
+        
+        log_background = np.random.normal(self.mean_background, 
+                                      self.sigma_background)
+        if z == 0:
+            x = np.exp(log_background)
+        else:
+            mean = np.log(self.mu * z)
+            log_signal = np.random.normal(mean, self.sigma)
+            
+            x = np.exp(log_signal) + np.exp(log_background)
+
+        return x
+            
 
     def p_x_i_given_z_i(self, x_i, z_i):
         '''
-        compute p( x | z).
+    #    compute p( x | z).
 
-        Args:
+     #   Args:
 
-            x_i (float):
-                the observed flourescence intensity at time i
+      #      x_i (float):
+       #         the observed flourescence intensity at time i
 
-            z_i (int):
-                the hidden state, or number of bound fluorophores at time i
-
-        '''
+       #     z_i (int):
+        #        the hidden state, or number of bound fluorophores at time i
+'''
 
         #mu = np.log(self.mu * z_i) # Eq 18,  DOI: Biophysj 106.101428
         
         if z_i == 0:
-            mu = 0
+            mu = self.mean_background
             sigma2 = self.sigma2_background
+            x = x_i
+            
         else:
             mu = np.log(self.mu * z_i)
             sigma2 = self.sigma2
+            x = x_i - np.exp(self.mean_background)
+            print(f'mu is {mu} x is {x}')
 
-        result = integrate.romberg(lambda x: self._log_normal(x_i, mu, sigma2),
-                                x_i -1/256, x_i)
         
+        #result = self._integrate_from_cdf(x, mu, sigma2)
+        result = integrate.romberg(lambda x: self._log_normal(x, mu, sigma2),
+                                   x, x + (1/256))
         return result
+
 
     def _log_normal(self, x, mu, sigma2):
 
+        return 1.0 / (np.sqrt(2.0 * np.pi * sigma2)) * \
+                np.exp(-(x - mu)**2/(2.0 * sigma2))
+                
+    def _normal(self, x, mu, sigma2):
+        
         return 1.0 / (x * np.sqrt(2.0 * np.pi * sigma2)) * \
                 np.exp(-(np.log(x) - mu)**2/(2.0 * sigma2))
                 
     
     def approx_phi(self, x, mu, sigma):
         ''' 
-        - method to approximate the integral value of p_x_i_given z_i
-        -from Zelen & Severo approximation of the standard Normal CDF
-        - Does not work for large differences in x and mu
-        '''
+  #      - method to approximate the integral value of p_x_i_given z_i
+   #     -from Zelen & Severo approximation of the standard Normal CDF
+    #    - Does not work for large differences in x and mu
+'''
         b_consts = [0.2316419, 0.319381530, -0.356563782, 1.781477937,
                        -1.821255978, 1.330274429 ]
         
@@ -122,17 +200,53 @@ class FluorescenceModel:
             
         return phi
         
-    def integrate_from_cdf(self, x, mu, sigma):
+    def _integrate_from_cdf(self, x, mu, sigma):
         ''' 
-        - method to approximate the integral value of p_x_i_given z_i
-        - Does not work for large differences in x and mu with a small sigma
-        '''
-        a = stats.lognorm.pdf(x, sigma, mu)
-        b = stats.lognorm.pdf(x + (1/256), sigma, mu)
+ #       - method to approximate the integral value of p_x_i_given z_i
+  #      - Does not work for large differences in x and mu with a small sigma
+'''
+        a = self._log_normal(x, mu, sigma**2)
+        b = self._log_normal(x+ (1/256), mu, sigma**2)
         
-        prob = a-b
+        prob = np.abs(a-b)
         return prob
         
     
+def log_normal(x, mu, sigma2):
 
+    return 1.0 / (x * np.sqrt(2.0 * np.pi * sigma2)) * \
+            np.exp(-(np.log(x) - mu)**2/(2.0 * sigma2))
+            
+def integrate_from_cdf(x, mu, sigma2):
+    ''' 
+ #   - method to approximate the integral value of p_x_i_given z_i
+  #  - Does not work for large differences in x and mu with a small sigma
+'''
+    a = log_normal(x, mu, sigma2)
+    b = log_normal(x+ (1/256), mu, sigma2)
     
+    prob = np.abs(a-b)
+    return prob
+            
+x = np.linspace(0,10,1000)
+y = np.zeros((len(x)))
+mu = 0
+sigma2 = 0.01
+for i in range(len(x)):
+    y[i] = normal(x[i],mu, sigma2)
+plt.plot(x,y)
+
+xs = np.zeros((1000,4))
+ys = np.zeros((1000,4))
+count = 0
+for j in range(xs.shape[1]):
+    for i in range(xs.shape[0]):
+        ys[i,j] = f_model.norm_sample_x_i_given_z_i(count)
+    count += 1
+    
+def normal(x, mu, sigma2):
+    
+    return 1.0 / (x * np.sqrt(2.0 * np.pi * sigma2)) * \
+            np.exp(-(np.log(x) - mu)**2/(2.0 * sigma2))
+            
+'''
--- for_promap/test.py
+++ for_promap/test.py
@@ -1,27 +1,12 @@
 import numpy as np
 import unittest
 import pandas as pd
-from fit_real_data import read_image, clean_spots, process_image
-from calibrate import normalize_trace, extract_traces
+#from fit_real_data import read_image, clean_spots, process_image
+import calibrate
+from calibrate import normalize_trace, extract_traces, process_image
 from trace_model import TraceModel
 from fluorescence_model import ModelParams
 
-class TestFitRealData(unittest.TestCase):
-
-    def test_fits(self):
-        image_file_path = '../../Images/0525_5nM_1.tif'
-        spots_file_path = '../../Images/0525_5_nM_1_spots.csv'
-        
-        img = read_image(image_file_path)
-        spots = pd.read_csv(spots_file_path)
-        yx_spots = clean_spots(spots, img)
-        
-        test_spots = yx_spots[np.random.randint(0, yx_spots.shape[0], 2)]
-        p_values = process_image(img, test_spots)
-        
-        # check probabilites are between 0 and 1
-        self.assertGreaterEqual(np.min(p_values), 0)
-        self.assertLessEqual(np.max(p_values), 1)
 
 
 class TestCalibration(unittest.TestCase):
@@ -30,7 +15,7 @@ class TestCalibration(unittest.TestCase):
        image_file_path = '../../Images/0525_5nM_1.tif'
        spots_file_path = '../../Images/0525_5_nM_1_spots.csv'
        
-       img = read_image(image_file_path)
+       img = calibrate.read_image(image_file_path)
        spots = pd.read_csv(spots_file_path)
        
        all_traces = extract_traces(img, spots)
@@ -43,8 +28,22 @@ class TestCalibration(unittest.TestCase):
           
           # check that trace is shifted to mean 1
           self.assertAlmostEqual(np.mean(scaled_trace), 1, delta = 0.1)
-       
-
+"""       
+   def test_fits(self):
+        image_file_path = '../../Images/0525_5nM_1.tif'
+        spots_file_path = '../../Images/0525_5_nM_1_spots.csv'
+        
+        img = calibrate.read_image(image_file_path)
+        spots = pd.read_csv(spots_file_path)
+        yx_spots = calibrate.clean_spots(spots, img)
+        
+        test_spots = yx_spots[np.random.randint(0, yx_spots.shape[0], 2)]
+        p_values = process_image(img, test_spots)
+        
+        # check probabilites are between 0 and 1
+        self.assertGreaterEqual(np.min(p_values), 0)
+        self.assertLessEqual(np.max(p_values), 1)
+"""
 class TestTraceModel(unittest.TestCase):
     def test_generate_trace(self):
         sim_trace_len = 100
